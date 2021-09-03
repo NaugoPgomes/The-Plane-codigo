63838b1f3c00995ccb1659a5bb30a4141e4c03ab
Naugo Peixoto Gomes <80015739+NaugoPgomes@users.noreply.github.com>
2021-09-03 18:29:21 -0300

    Add files via upload
    adicionei um código para poder mutar e desmutar o som do jogo.

---
+++ codigos/SongController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SongController : MonoBehaviour
+{
+
+    private bool Song = true;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void SongVolume()
+    {
+        Song = !Song;
+        if(Song == true)
+        {
+            AudioListener.volume = 1;
+        }
+        else
+        {
+            AudioListener.volume = 0;
+        }
+    }
+}
