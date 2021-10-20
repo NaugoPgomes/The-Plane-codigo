6aa7049242750a56c89eaa6489705b37e403b43c
naugo <naugopgomes@gmail.com>
2021-10-20 10:23:57 -0300

    adicionando um botão na tela de game over, que vai levar para a tela inicial do jogo

---
+++ codigos/GoToHomeScreen.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToHomeScreen : MonoBehaviour
+{
+    
+    public void HomeScreen ()
+    {
+        SceneManager.LoadScene("HomeScreen");
+    }
+
+}
